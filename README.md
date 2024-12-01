cee7d724da729a516b7cab5ff70659804d2375c2
guowei <guowei@qq.com>
2024-12-02 00:19:48 +0800

    单词格式转换

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # go-programming-tour-book
 打造属于自己的工具集
+
+## 单词格式转换 -- word
+> 基于但三方开源库Cobra和标准库strings、unicode 实现了多种模式的单词转换功能
+
+### 验证
+```go
+
+go run main.go help  word
+
+该子命令支持各种单词格式转换，模式如下：
+1: 全部单词转为大写
+2: 全部单词转为小写
+3: 下画线单词转为大写驼峰单词
+4: 下画线单词转为小写驼峰单词
+5: 驼峰单词转为下画线单词
+
+Usage:
+   word [flags]
+
+Flags:
+  -h, --help         help for word
+  -m, --mode int8    请输入单词转换模式
+  -s, --str string   请输入单词内容
+
+```
+```go
+go run main.go word -s=xiaowei -m=1
+输出结果: XIAOWEI
+
+go run main.go word -s=XIAOWEI -m=2
+输出结果: xiaowei
+
+go run main.go word -s=xiaowei -m=3
+输出结果: Xiaowei
+
+go run main.go word -s=XIAOWEI -m=4
+输出结果: xIAOWEI
+
+go run main.go word -s=XiaoWei -m=5
+输出结果: xiao_wei
+
+```
--- cmd/word.go
+++ cmd/word.go
@@ -1,15 +1,61 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"log"
+	"strings"
+
+	"github.com/go-programming-tour-book/tour/internal/word"
+	"github.com/spf13/cobra"
+)
+
+const (
+	ModeUpper                      = iota + 1 // 全部单词转为大写
+	ModeLower                                 // 全部单词转为小写
+	ModeUnderscoreToUpperCamelCase            // 下画线单词转为大写驼峰单词
+	ModeUnderscoreToLowerCamelCase            // 下画线单词转为小写驼峰单词
+	ModeCamelCaseToUnderscore                 // 驼峰单词转为下画线单词
+)
+
+var desc = strings.Join([]string{
+	"该子命令支持各种单词格式转换，模式如下：",
+	"1: 全部单词转为大写",
+	"2: 全部单词转为小写",
+	"3: 下画线单词转为大写驼峰单词",
+	"4: 下画线单词转为小写驼峰单词",
+	"5: 驼峰单词转为下画线单词",
+}, "\n")
 
 // 单词格式转换的子命令word
 
 var wordCmd = &cobra.Command{
 	Use:   "word",
 	Short: "单词格式转换",
-	Long:  "支持多种单词格式转换",
+	Long:  desc,
 	Run: func(cmd *cobra.Command, args []string) {
+		var content string
+
+		switch mode {
+		case ModeUpper:
+			content = word.ToUpper((str))
+		case ModeLower:
+			content = word.ToLower(str)
+		case ModeUnderscoreToUpperCamelCase:
+			content = word.UnderscoreToUpperCamelCase(str)
+		case ModeUnderscoreToLowerCamelCase:
+			content = word.UnderscoreToLowerCamelCase(str)
+		case ModeCamelCaseToUnderscore:
+			content = word.CamelCaseToUnderscore(str)
+		default:
+			log.Fatalf("暂不支持该模式转换, 请执行 help word 查看帮助文档")
+		}
+		log.Printf("输出结果: %s", content)
 	},
 }
 
-func init() {}
+var str string
+var mode int8
+
+func init() {
+	wordCmd.Flags().StringVarP(&str, "str", "s", "", "请输入单词内容")
+	wordCmd.Flags().Int8VarP(&mode, "mode", "m", 0, "请输入单词转换模式")
+}
